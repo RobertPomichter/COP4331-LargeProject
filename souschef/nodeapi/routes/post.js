4cf6b0a43344f1d4cb6aa2a69a2b0e88a14aac39
Cydni Turner <cydturner01@gmail.com>
2021-04-01 19:13:41 -0400

    routes added

---
+++ souschef/nodeapi/routes/post.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { signup, 
+        signin, 
+        signout, 
+        signInRequired,
+        forgotPassword,
+        resetPassword } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { userSignupValidation,
+        passwordResetValidator } = require('../validators/index');
+
+const router = express.Router(); 
+
+// sign up route
+router.post('/signup', userSignupValidation, signup);
+
+// sign in route
+router.post('/signin', signin);
+
+// sign out route
+router.get('/signout', signInRequired, signout); 
+
+// password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
+
+// any request with userId will go through middleware
+router.param('userId', userById);
+
+module.exports = router;
