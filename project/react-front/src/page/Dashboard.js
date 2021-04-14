423d83d74c19bb54610652a0005eaae13f8f3785
Octavius <yveltcl@gmail.com>
2021-04-13 20:47:16 -0400

    init component files

---
+++ project/react-front/src/page/Dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MainRouter from './MainRouter';
+import axios from 'axios';
+
+import {
+    Button, 
+    Card, 
+    CardHeader,
+    CardBody,
+    CardTitle,
+    DropdownMenu,
+    DropdownItem,
+    Label,
+    FormGroup,
+    Row,
+    Col,
+    UncontrolledDropdown,
+} from "reactstrap";
+
+class Dashboard extends Component {
+  render() {
+    return <div></div>
+  }
+  
+}
+
+export default Dashboard
