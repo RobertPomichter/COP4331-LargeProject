77c73df7d8a1ff8e74646911a4e279ebcfe7f061
Max Gomer <maximilliangomer@gmail.com>
2021-04-15 03:01:35 -0400

    FE-Small Inventory Testing Update
    Not much to update, just some basic lines I tried using to test API calls that I don't have working yet. Small Ingredient SCSS edit. New folder & file for API calling functions for Inventory Panel

--- project/react-front/src/core/InventoryPanel.js
+++ project/react-front/src/core/InventoryPanel.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-
+import { Button } from'@material-ui/core';
+import { getAllIngredients } from '../apiCalls/apiInventory.js';
 
 class InventoryPanel extends Component {
 
+    clickGetAllIngredients = event => {
+        // prevent default page reload
+        event.preventDefault();
+
+        getAllIngredients();
+    }
+
     render() {
         return (
-            <div className='jumbotron'>
-                <h1>Inventory Testing Area</h1>
+            <div>
+                <div className='jumbotron'>
+                    <h1>Inventory Testing Area</h1>
+                </div>
+                <div className="getIngredientsContainer">
+                    <Button variant="contained" onClick={this.clickGetAllIngredients}>
+                        Test: Get All Ingredients
+                    </Button>
+                </div>
             </div>
         );
     }
