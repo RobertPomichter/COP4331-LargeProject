0ebcae2c97ad58d7be888c860c2fec5cb0cc9c81
Max Gomer <maximilliangomer@gmail.com>
2021-04-14 23:45:24 -0400

    FE-Ingredient Component Progress
    Add _IngredientStyles.scss to style each ingredient component, start building ingredient component using styled circular elements and images

--- project/react-front/src/app/Ingredient.js
+++ project/react-front/src/app/Ingredient.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
 import GenericMeatPicture from '../images/GenericMeat.jpg';
 
 // The component that contains the UI for a single ingredient
@@ -7,7 +8,14 @@ class Ingredient extends Component {
 
     render() {
         return (
-            <img className='ingredient' src={GenericMeatPicture}></img>
+            <div>
+                <h3>This image was made using HTML/JSX tags</h3>
+                <div className='ingredientCircleContainer'>
+                    <img className='ingredientImage' src={GenericMeatPicture}></img>
+                </div>
+                
+            </div>
+            
         );
     }
 }
