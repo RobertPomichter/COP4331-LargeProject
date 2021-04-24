b833d84fb1ff9f3bac1fcb6e5c607805b935edd1
Max Gomer <maximilliangomer@gmail.com>
2021-04-24 05:21:17 -0400

    Quick ClassName fix
    that's it :)

--- project/react-front/src/app/EmptyCategoryDisplay.js
+++ project/react-front/src/app/EmptyCategoryDisplay.js
@@ -9,7 +9,7 @@ class EmptyCategoryDisplay extends Component {
     render() {
         return (
             <div className='emptyFoodCategoryContainer'>
-                <img className='ingredientImage' src={EmptyFoodCategory}></img>
+                <img className='emptyCategoryImage' src={EmptyFoodCategory}></img>
             </div>
         );
     }
