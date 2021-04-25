e09366e2fc55734a68ace1732f7fab300b87abb5
Jordan <jecollins2019@knights.ucf.edu>
2021-04-24 21:18:47 -0400

    we're trying

---
+++ nodeapi/controllers/ingredient.js
@@ -0,0 +1,165 @@
+// import packages
+const Ingredient = require('../models/ingredient'); // import User from "../models/user"
+const { db } = require('../models/ingredient');
+const user = require('../models/user');
+const _ = require('lodash');
+const fs = require('fs');
+const formidable = require('formidable');
+
+exports.ingredientById = (req, res, next, id) => {
+
+    // find the ingredient in db by their id
+    Ingredient.findById(id).exec((err, ingredient) => {
+        // if there is an error or user is not found
+        if(err || !user){
+            //return the 400 status and send error message
+            return res.status(400).json({
+                error: "Ingredient not found"
+            });
+        }
+
+        // otherwise the ingredient was found
+        // add the profile object to the req
+        // give the progile object the user
+        req.profile = ingredient;
+
+        // go to next
+        next();
+    })
+}
+
+exports.addIngredient = async (req, res) => {
+    // check if ingredient exists via similar robert code in auth.js (don't forget comma for name and user_email)
+
+    // Check and see if the ingredient being added is already in the ingredient list
+    const ingredientExists = await Ingredient.findOne({email: req.body.email}, {name: req.body.name}); //{email: req.body.email}, {
+	if(ingredientExists) 
+        return res.status(403).json({ 
+		    error: "That food already exists as an ingredient"
+	    });
+
+    // create the new ingredient
+    const ingredient = await new Ingredient(req.body);
+    //save the new ingredient
+    await ingredient.save();
+    res.status(200).json({message: "Ingredient Added!"});
+}
+
+exports.getUserIngredients = (req, res) => {
+    console.log("Beginning of getUserIngredients");
+    const email = req.profile.email;
+
+    db.collection("ingredients").find({user_email: email}).toArray(function(err, result) {
+        if (err) {
+            return res.json({error: "Error fetching ingredients from the db"}); 
+        }
+        console.log("Ingredients fetched successfully.");
+        return res.json(result);
+    });
+}
+
+exports.getUserIngredientsByCategory = (req, res) => {
+    console.log("Beginning of getUserIngredients");
+    const email = req.profile.email;
+    const category = req.query.cat;
+
+    db.collection("ingredients").find({user_email: email, category: category}).toArray(function(err, result) {
+        if (err) {
+            return res.json({error: "Error fetching ingredients from the db"}); 
+        }
+        console.log("Ingredients fetched successfully.");
+        return res.json(result);
+    });
+}
+
+exports.getUserIngredientsByCategoryDontHave = (req, res) => {
+    console.log("Beginning of getUserIngredients");
+    const email = req.profile.email;
+    const category = req.query.cat;
+
+    db.collection("ingredients").find({user_email: email, category: category, amount: 0}).toArray(function(err, result) {
+        if (err) {
+            return res.json({error: "Error fetching ingredients from the db"}); 
+        }
+        console.log("Ingredients fetched successfully.");
+        return res.json(result);
+    });
+}
+
+exports.getUserIngredientsByCategoryHave = (req, res) => {
+    console.log("Beginning of getUserIngredients");
+    const email = req.profile.email;
+    const category = req.query.cat;
+
+    db.collection("ingredients").find({user_email: email, category: category, amount: { $ne : 0 } }).toArray(function(err, result) {
+        if (err) {
+            return res.json({error: "Error fetching ingredients from the db"}); 
+        }
+        console.log("Ingredients fetched successfully.");
+        return res.json(result);
+    });
+}
+
+// custom querry currently under construction
+exports.getUserIngredientsQuery = (req, res) => {
+    console.log("Beginning of getUserIngredients");
+    const query = req.query;
+
+    // dont forget to also pass the email in this query
+    db.collection("ingredients").find(query).toArray(function(err, result) {
+        if (err) {
+            return res.json({error: "Error fetching ingredients from the db"}); 
+        }
+        console.log("Ingredients fetched successfully.");
+        return res.json(result);
+    });
+}
+
+exports.updateIngredients = (req, res, next) => {
+    // grab the form
+
+    let form = new formidable.IncomingForm();
+    
+    // keep the extensions
+    form.keepExtensions = true;
+
+    //parse the form
+    form.parse(req, (err, fields, files) => {
+        // handle any errors
+        if(err){
+            return res.status(400).json({
+                error: "Failed to upload file"
+            });
+        }
+
+        // get the ingredient from the request profile (from ingredientById function)
+        let ingredient = req.profile;
+
+        // use lodash extends function to update user
+        // with data from form fields (name, email, etc.)
+        ingredient = _.extend(ingredient, fields);
+
+        // update the user updated property
+        ingredient.updated = Date.now();
+
+        // if there is a photo in the files of the form
+        if(files.photo){
+            // set the photo data of the user object
+            ingredient.photo.data = fs.readFileSync(files.photo.path);
+            ingredient.photo.contentType = files.photo.type;
+        }
+
+        // save the ingredient to the db
+        ingredient.save( (err, result) => {
+            // handle any errors
+            if(err){
+                return res.status(400).json({
+                    error: "An error occured saving updates to the database."
+                });
+            }
+
+            // if no errors send json response
+            res.json(ingredient);
+        });
+    });
+}
---
+++ nodeapi/routes/ingredient.js
@@ -0,0 +1,48 @@
+// package imports
+const express = require('express');
+
+const {
+    allIngredients,
+    addIngredient,
+    getUserIngredients,
+    getUserIngredientsByCategory,
+    getUserIngredientsByCategoryHave,
+    getUserIngredientsByCategoryDontHave,
+    getUserIngredientsQuery,
+    updateIngredients,
+    ingredientById
+} = require('../controllers/ingredient');
+
+const { 
+    signInRequired 
+} = require('../controllers/auth');
+
+const { 
+    userById
+} = require('../controllers/user');
+
+// create router using the express package
+const router = express.Router();
+
+// any request with userId will go through middleware
+router.param('userId', userById);
+router.param('ingredientId', ingredientById);
+
+// route to get all ingredients
+//router.get('/ingredients', allIngredients);
+// route to add an ingredient
+router.post('/ingredient', addIngredient);
+// route to get a user's ingredients
+router.get('/ingredient/:userId', getUserIngredients);
+// route to get user's ingredients of a certain category
+router.get('/ingredientCategory/:userId', getUserIngredientsByCategory);
+// route to get user's ingredients of a certain category they have in stock
+router.get('/ingredientCategoryHave/:userId', getUserIngredientsByCategoryHave);
+// route to get user's ingredients of a certain category they are out of
+router.get('/ingredientCategoryDont/:userId', getUserIngredientsByCategoryDontHave);
+// route to get user's ingridents by a custom front end query
+router.get('/ingredientQuery/:userId', getUserIngredientsQuery);
+// route to put an ingredient update
+router.put('/ingredientUpdate/:ingredientId', updateIngredients); 
+
+module.exports = router;
