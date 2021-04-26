8166c315867772656005686aa4e013ab650afacb
Max Gomer <maximilliangomer@gmail.com>
2021-04-26 16:16:00 -0400

    IngredientState Setup & Edit Framework Setup
    Change single ingredient category component (just meat component at the moment) to use a state that contains its parameters. Use the previously passed props to set the ingredient's state.

--- project/react-front/src/app/IngredientMeat.js
+++ project/react-front/src/app/IngredientMeat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, InputLabel, MenuItem, FormHelperText, FormControl, Select } from '@material-ui/core';
 import Image from 'react-bootstrap/Image';
 import GenericMeatPicture from '../images/GenericMeat.jpg';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,17 +10,24 @@ import { isAuthenticated } from "../auth";
 
 
 class IngredientMeat extends Component {
-
+    // copied from Users.js - idea is to hold an array of ingredients
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            unit: "",
+            amount: "",
+            showAddForm: false,
+        }
+    }
     // function to handle deleting this ingredient
     clickDeleteSelf = event => {
         // gather information to send to API
         const name = this.props.meatName;
-        const user_email = this.props.user_email;
         const userId = this.props.userId;
 
         const deleteIngredientPackage = {
-            name,
-            user_email
+            name
         }
 
         // get the token
@@ -35,25 +43,66 @@ class IngredientMeat extends Component {
         });
     }
 
+    // method to set internal state from passed in props
+    componentDidMount() {
+        this.setState({ name: this.props.meatName,
+                        unit: this.props.meatUnit,
+                        amount: this.props.meatAmount});
+    }
+
+    // function to update addIngredient relevant state variables
+    handleChange = (stateVariableToChange) => event => {
+        this.setState({[stateVariableToChange] : event.target.value});
+    }
+
+    handleClose = () => this.setState({ showAddForm: false });
+    handleShow = () => this.setState({ showAddForm: true });
+
     render() {
         return (
-            <div className='ingredientContainer'>
-                <div className='ingredientMeatCircleContainer'>
-                    <img className='ingredientImage' src={GenericMeatPicture}></img>
-                </div>
-                <span className='meatName'>{this.props.meatName}</span>
-                <br/>
-                <span className='meatAmount'>{this.props.meatAmount}</span>
-                {" "}
-                <span className='meatUnit'>{this.props.meatUnit}</span>
-                <br />
-                <IconButton aria-label="delete" onClick={this.clickDeleteSelf}>
-                    <DeleteTwoToneIcon />
-                </IconButton>
-                <IconButton aria-label="edit">
-                    <EditTwoToneIcon />
-                </IconButton>
-            </div>
+                <div className='ingredientContainer'>
+                    {/* EditIngredient Dialog Form (a better form of Modal) */}
+                    <Dialog open={this.state.showAddForm} onClose={this.handleClose} disableScrollLock='true'
+                    aria-labelledby="form-dialog-title">
+                        <DialogTitle id="form-dialog-title">Edit Ingredient</DialogTitle>
+                        <DialogContent>
+                        <DialogContentText>Please fill out the following fields</DialogContentText>
+                        <TextField margin="dense" label="Ingredient Name" fullWidth
+                                onChange={this.handleChange("name")}
+                                value={this.state.name}/>
+                        <TextField margin="dense" label="Unit of Measurement" fullWidth
+                                onChange={this.handleChange("unit")}
+                                value={this.state.unit}/>
+                        <TextField margin="dense" label="Amount" fullWidth
+                                onChange={this.handleChange("amount")}
+                                value={this.state.amount}/>
+                        <TextField margin="dense" label="Reminder to check adding a photo" fullWidth/>
+                        </DialogContent>
+                        <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleAddSubmit} color="primary">
+                            Submit
+                        </Button>
+                        </DialogActions>
+                    </Dialog>
+                    <div className='ingredientMeatCircleContainer'>
+                        <img className='ingredientImage' src={GenericMeatPicture}></img>
+                    </div>
+                    <span className='meatName'>{this.state.name}</span>
+                    <br/>
+                    <span className='meatAmount'>{this.state.amount}</span>
+                    {" "}
+                    <span className='meatUnit'>{this.state.unit}</span>
+                    <br />
+                    <IconButton aria-label="delete" onClick={this.clickDeleteSelf}>
+                        <DeleteTwoToneIcon />
+                    </IconButton>
+                    <IconButton aria-label="edit" onClick={this.handleShow}>
+                        <EditTwoToneIcon />
+                    </IconButton>
+                </div>         
         );
     }
 }
